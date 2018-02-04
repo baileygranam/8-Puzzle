32516f10cb34d25ca1f514dac45c3f1100814ad2
Bailey Granam <bgranam@stetson.edu>
2018-02-04 17:58:02 -0500

    Added ability to swap elements in table.

--- puzzle_script.js
+++ puzzle_script.js
@@ -1,10 +1,14 @@
+var table = document.getElementById("tableID");
+var x = 1;
+var y = 1;
+
 window.onload = function() {
-    var table = document.getElementById("tableID");
     if (table != null) {
         for (var i = 0; i < table.rows.length; i++) {
             for (var j = 0; j < table.rows[i].cells.length; j++) {
                 table.rows[i].cells[j].onclick = function () {
-                    tableText(this, this.cellIndex+1, this.parentNode.rowIndex+1);
+                    tableText(this, this.parentNode.rowIndex, this.cellIndex);
+                    swap(this, this.parentNode.rowIndex, this.cellIndex);
                 };
             }
         }
@@ -13,4 +17,20 @@ window.onload = function() {
 
 function tableText(tableCell, i, j) {
     alert("Number: " + tableCell.innerHTML + " " +"Column: " + i + " " +"Row: " + j);
+}
+
+function swap(tableCell, i , j)
+{
+    if(validateSwap(i, j))
+    {
+        alert(validateSwap(i, j));
+        table.rows[x].cells[y].innerHTML = table.rows[i].cells[j].innerHTML;
+        table.rows[i].cells[j].innerHTML = "";
+        x = i;
+        y = j;
+    }
+    else
+    {
+        alert("ILLEGAL MOVE");
+    }
 }
